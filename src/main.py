0729a1dc826974145c3b02bc88c6114d907f3cbe
Rafael Neto Maia <rafaelnetomaiar@gmail.com>
2026-03-09 13:13:34 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,27 +12,27 @@ while True:
     
         elif operador == '-':
             resultado = primeiro_numero - segundo_numero
-            print(f'{primeiro_numero} - {segundo_numero} = {resultado}.')
+            print(f'{primeiro_numero} - {segundo_numero} = {resultado}. ')
     
         elif operador == '/':
             if segundo_numero == 0:
-                print('Divisão com o segundo número sendo zero não é permitido.')
+                print('Divisão com o segundo número sendo zero não é permitido. ')
             else:
                 resultado = primeiro_numero / segundo_numero
-                print(f'{primeiro_numero} / {segundo_numero} = {resultado}.')
+                print(f'{primeiro_numero} / {segundo_numero} = {resultado}. ')
 
         elif operador == '*':
             resultado = primeiro_numero * segundo_numero
-            print(f'{primeiro_numero} * {segundo_numero} = {resultado}.')
+            print(f'{primeiro_numero} * {segundo_numero} = {resultado}. ')
 
         else:
             print('Operador inválido! Use +, -, / ou *.')
 
     except ValueError:
-        print('Valor digitado inválido!')
+        print('Valor digitado inválido! ')
         continue # Volta ao início do loop sem perguntar para sair
 
     sair = input('Você deseja sair? [s]im: ').lower().startswith('s')
     if sair:  
-        print('Saindo da calculadora.')
+        print('Saindo da calculadora. ')
         break
