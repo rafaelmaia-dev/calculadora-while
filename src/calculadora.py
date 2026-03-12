09e96b755b647243207e605ad865e124f55086a5
Rafael Neto Maia <rafaelnetomaiar@gmail.com>
2026-03-12 10:52:16 -0300

    Add files via upload

---
+++ src/calculadora.py
@@ -0,0 +1,23 @@
+from operacoes import operacao_aritmetica
+
+def main():
+    while True:
+        try:
+            n1 = float(input('Digite um número: '))
+            n2 = float(input("Digite outro número: "))
+            operador = input("Digite um dos operadores ao lado (+, -, *, /): ").strip()
+
+            resultado = operacao_aritmetica(n1, n2, operador)
+            print(f'{n1} {operador} {n2} = {resultado} ')
+
+        except ValueError as e:
+            print(f'Erro: {e} ')
+
+        sair = input("Deseja sair? [s]im: ").strip().lower()
+        if sair in ('s, sim'):
+            print('Saindo da calculadora. ')
+            break
+
+main()
+
+        
